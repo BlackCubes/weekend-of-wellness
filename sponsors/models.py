e69a3d6882385ad4d206ce6f7aac33316b3f2389
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-02-24 00:49:11 -0800

    feat(models-sponsors): add model with error messages

--- sponsors/models.py
+++ sponsors/models.py
@@ -1,3 +1,24 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+from .utils import model_error_messages
+
+
+class Sponsors(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        error_messages=model_error_messages["sponsors"]["name"],
+    )
+    website = models.URLField(
+        max_length=200,
+        error_messages=model_error_messages["sponsors"]["website"],
+    )
+    image = models.ImageField(
+        upload_to="sponsors",
+        error_messages=["sponsors"]["image"],
+    )
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
---
+++ sponsors/utils.py
@@ -0,0 +1,21 @@
+model_error_messages = {
+    "sponsors": {
+        "name": {
+            "blank": "The name cannot be blank.",
+            "invalid": "Invalid value for the name.",
+            "max_length": "The name must be 100 characters or less.",
+            "null": "The name cannot be empty.",
+            "required": "The name is required.",
+            "unique": "The name must be unique.",
+        },
+        "image": {
+            "blank": "The image cannot be empty.",
+            "required": "The image is required.",
+        },
+        "website": {
+            "blank": "The website cannot be blank.",
+            "max_length": "The website must be 200 characters or less.",
+            "required": "The website is required.",
+        },
+    },
+}
