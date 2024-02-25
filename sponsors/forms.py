a8f655e1cbbadf7f36189916fb961e5c47de051a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-02-24 21:46:45 -0800

    feat(forms-sponsors): add file and create form for sponsors

---
+++ sponsors/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+
+from .models import Sponsors
+from .utils import model_error_messages
+
+
+class SponsorsForm(forms.ModelForm):
+    class Meta:
+        model = Sponsors
+
+        fields = (
+            "name",
+            "website",
+            "image",
+        )
+
+        labels = {"name": "*Name", "website": "*Website", "image": "*Image"}
+
+        widgets = {
+            "name": forms.TextInput(attrs={"placeholder": "Sponsor's name"}),
+            "website": forms.TextInput(attrs={"placeholder": "Sponsor's website"}),
+        }
+
+        error_messages = {
+            "name": model_error_messages["sponsors"]["name"],
+            "website": model_error_messages["sponsors"]["website"],
+            "image": model_error_messages["sponsors"]["image"],
+        }
