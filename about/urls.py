7e1058ac67943578214db61a6ea87685637d39cb
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-03-01 17:39:38 -0800

    feat(urls-contact-about): route the contact page

--- about/urls.py
+++ about/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import DirectionsTemplateView, FaqListView
+from .views import ContactTemplateView, DirectionsTemplateView, FaqListView
 
 app_name = "about"
 urlpatterns = [
     path("faq/", FaqListView.as_view(), name="faq"),
     path("directions/", DirectionsTemplateView.as_view(), name="directions"),
+    path("contact/", ContactTemplateView.as_view(), name="contact"),
 ]
