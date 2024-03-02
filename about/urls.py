87a4552735ec177c084e3eb168618893765090ed
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-03-01 17:44:17 -0800

    feat(urls-event-team): route event team page

--- about/urls.py
+++ about/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 
-from .views import ContactTemplateView, DirectionsTemplateView, FaqListView
+from .views import (
+    ContactTemplateView,
+    DirectionsTemplateView,
+    EventTeamTemplateView,
+    FaqListView,
+)
 
 app_name = "about"
 urlpatterns = [
     path("faq/", FaqListView.as_view(), name="faq"),
     path("directions/", DirectionsTemplateView.as_view(), name="directions"),
     path("contact/", ContactTemplateView.as_view(), name="contact"),
+    path("event-team/", EventTeamTemplateView.as_view(), name="event-team"),
 ]
