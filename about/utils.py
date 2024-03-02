43520d940de0cccc69ffa1deb66887e74e2ac11f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-03-01 16:30:44 -0800

    feat(utils-about): add error messages for the FAQ model

---
+++ about/utils.py
@@ -0,0 +1,18 @@
+model_error_messages = {
+    "faq": {
+        "question": {
+            "blank": "The question cannot be blank.",
+            "invalid": "Invalid value for the question.",
+            "max_length": "The question must be 100 characters or less.",
+            "null": "The question cannot be empty.",
+            "required": "The question is required.",
+        },
+        "answer": {
+            "blank": "The answer cannot be blank.",
+            "invalid": "Invalid value for the answer.",
+            "max_length": "The answer must be 300 characters or less.",
+            "null": "The answer cannot be empty.",
+            "required": "The answer is required.",
+        },
+    },
+}
