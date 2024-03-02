6a9536a6f8893ee7d99e23af4a5cd22e19aa2d84
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-03-01 17:31:01 -0800

    feat(urls-directions-about): route the directions page

--- about/urls.py
+++ about/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import FaqListView
+from .views import DirectionsTemplateView, FaqListView
 
 app_name = "about"
 urlpatterns = [
     path("faq/", FaqListView.as_view(), name="faq"),
+    path("directions/", DirectionsTemplateView.as_view(), name="directions"),
 ]
