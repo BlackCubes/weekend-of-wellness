7f516674b4422f6fbe4485122f2c8c7455d418a3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-03-01 18:14:25 -0800

    feat(views-events): render events, fitness classes, health fair, farmers market, pickleball tournament, and workshops pages

--- events/views.py
+++ events/views.py
@@ -1,3 +1,25 @@
-# from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class EventsTemplateView(TemplateView):
+    template_name = "events/index.html"
+
+
+class FitnessClassesTemplateView(TemplateView):
+    template_name = "events/fitness-classes.html"
+
+
+class HealthFairTemplateView(TemplateView):
+    template_name = "events/health-fair.html"
+
+
+class FarmersMarketTemplateView(TemplateView):
+    template_name = "events/farmers-market.html"
+
+
+class PickleballTournamentTemplateView(TemplateView):
+    template_name = "events/pickleball-tournament.html"
+
+
+class WorkshopsTemplateView(TemplateView):
+    template_name = "events/workshops.html"
