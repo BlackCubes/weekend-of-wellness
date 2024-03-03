15f55eee34f5468c05788355b7a600ef5071a37c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-03-03 12:38:30 -0800

    refactor(base-settings-core): change STATIC_ROOT

--- core/settings/base.py
+++ core/settings/base.py
@@ -101,8 +101,8 @@ STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 )
-STATICFILES_DIRS = (os.path.join(PROJECT_DIR, "static"),)
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATIC_ROOT = "/static/"
 STATIC_URL = "/static/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
