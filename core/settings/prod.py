870b1cc58a487d4dff59968d132916c7b87e054b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-03-18 22:19:17 -0700

    prod ready

---
+++ core/settings/prod.py
@@ -0,0 +1,45 @@
+from .base import *
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = False
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT"),
+    }
+}
+
+ALLOWED_HOSTS = (
+    "blackcubes.pythonanywhere.com",
+    "www.blackcubes.pythonanywhere.com",
+)
+
+CORS_ALLOWED_ORIGINS = [
+    "https://eliastgutierrez.com",
+    "https://www.eliastgutierrez.com",
+]
+
+CORS_ALLOW_METHODS = [
+    "GET",
+]
+
+# WHITENOISE
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = (
+    "compressor.filters.css_default.CssAbsoluteFilter",
+    "compressor.filters.cssmin.CSSMinFilter",
+)
+COMPRESS_CSS_HASHING_METHOD = "content"
+
+# HSTS SETTINGS
+SECURE_HSTS_SECONDS = 31536000  # 1 year
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
