3d88d0f0a28ebe5b429ba038c025c775154fd144
Elias Gutierrez <gutierrezelias1991@gmail.com>
2024-03-19 18:08:58 -0700

    change STATICFILES_DIRS

--- core/settings/base.py
+++ core/settings/base.py
@@ -102,7 +102,7 @@ STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 )
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(PROJECT_DIR, "static"),)
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
